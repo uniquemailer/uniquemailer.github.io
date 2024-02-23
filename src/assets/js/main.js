3b954903add2e58b1298030799a3dd368fb6a58c
Kemal Yenilmez <kemalyen@gmail.com>
2024-02-23 19:41:06 +0000

    init ...

---
+++ src/assets/js/main.js
@@ -0,0 +1,4 @@
+import Handlebars from "handlebars";
+const template = Handlebars.compile("Name: {{name}}");
+Handlebars.registerPartial('myPartial', '{{prefix}}');
+console.log(template({ name: "Nils" }));
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import vituum from 'vituum'
+import handlebars from '@vituum/vite-plugin-handlebars'
+import tailwindcss from '@vituum/vite-plugin-tailwindcss'
+
+export default {
+    plugins: [
+        vituum(),
+        tailwindcss(),
+        handlebars({
+            root: './src'
+        })
+    ]
+}
