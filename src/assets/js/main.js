0e38dc23ee4ee3f67bd4fbbfce7d7622120e1dcf
Kemal Yenilmez <kemalyen@gmail.com>
2024-02-25 11:55:18 +0000

    haf od the work

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,6 @@
 import Handlebars from "handlebars";
 const template = Handlebars.compile("Name: {{name}}");
 Handlebars.registerPartial('myPartial', '{{prefix}}');
+
+ 
 console.log(template({ name: "Nils" }));
