8d7523bf26d378c8b97d9e6dd6f830418987aa99
Kemal Yenilmez <kemalyen@gmail.com>
2024-02-28 16:20:22 +0000

    pages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,25 @@ Handlebars.registerPartial('myPartial', '{{prefix}}');
 
  
 console.log(template({ name: "Nils" }));
+(function () {
+    "use strict";
+
+  // ===== responsive navbar
+  let navbarToggler = document.querySelector("#navbarToggler");
+  const navbarCollapse = document.querySelector("#navbarCollapse");
+
+  navbarToggler.addEventListener("click", () => {
+    navbarToggler.classList.toggle("navbarTogglerActive");
+    navbarCollapse.classList.toggle("hidden");
+  });
+
+  //===== close navbar-collapse when a  clicked
+  document
+    .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
+    .forEach((e) =>
+      e.addEventListener("click", () => {
+        navbarToggler.classList.remove("navbarTogglerActive");
+        navbarCollapse.classList.add("hidden");
+      })
+    );
+})();
