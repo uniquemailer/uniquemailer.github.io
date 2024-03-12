e5b92270a1b28364f778d8499394895b1bd48e59
Kemal Yenilmez <kemalyen@gmail.com>
2024-03-12 21:33:03 +0000

    content placeholders

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,7 @@ export default {
             7: "#D1D5DB",
             8: "#E5E7EB",
           },
-          primary: "#3758F9",
+          primary: "#e11d48",
           "blue-dark": "#1B44C8",
           secondary: "#13C296",
           "body-color": "#637381",
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import vituum from 'vituum'
 import handlebars from '@vituum/vite-plugin-handlebars'
 import tailwindcss from '@vituum/vite-plugin-tailwindcss'
-
+import { normalizePath } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'    
 export default {
     plugins: [
+
+        viteStaticCopy({
+            targets: [
+              {
+                src: './src/assets/images',
+                dest: './assets/'
+              }
+            ]
+          }),
+
         vituum(),
         tailwindcss(),
         handlebars({
